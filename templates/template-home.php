c04a31a162bdaeab372dfa3ecb4be5565c86e50d
ryanrunqin <qinrun1989@gmail.com>
2018-01-11 01:50:08 +1300

    Added Meta Slider and Our Services section

--- templates/template-home.php
+++ templates/template-home.php
@@ -10,27 +10,32 @@
 get_header(); ?>
 
 
-
+<section class="home-slider">
+	<?php echo do_shortcode('[metaslider id="26"]'); ?>
+</section>
 
 <!-- services section -->
 <section id="services" class="services section">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-sm-12 col-xs-12">
-				<div class="section-title">
+				
 					<?php 
-						$page = get_posts(
+						$ourServicePage = get_posts(
 						    array( 'name'      => 'our-services',
 						           'post_type' => 'page'
 						       )
 							);
 					?>
-					<h1><?php echo get_the_title($page[0]->ID) ?></h1>
-				</div>
+					<?php 
+						if($ourServicePage)
+						{
+							echo "<div class='section-title'><h1>". get_the_title($ourServicePage[0]->ID) ."</h1></div>";
+							echo "<div class='row'><p>".$ourServicePage[0]->post_content."</p></div>";
+						}
+					?>
+				
 			</div>
-		</div>
-		<div class="row">
-			<p><?php echo $page[0]->post_content; ?></p>
 		</div>
 	</div>
 </section>
